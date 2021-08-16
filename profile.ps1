7d2807000b198ff482957b1535cfcfb1279c4c1c
Somo <somombo@users.noreply.github.com>
2021-08-15 20:00:36 -0400

    Create profile.ps1

---
+++ profile.ps1
@@ -0,0 +1,15 @@
+
+
+#### docker
+function docb { docker-compose build --force-rm }
+function docu { docker-compose up -d --remove-orphans }
+function docd { docker-compose down  --remove-orphans --rmi local }
+function docl { docker-compose logs -f }
+function doc { docd; docb; docu; docl }
+### end:docker
+
+#region conda initialize
+# !! Contents within this block are managed by 'conda init' !!
+(& "C:\tools\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
+#endregion
+
